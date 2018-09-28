f8c6980f6262b500da74ae2afafc7d3ece6ffd1d
Emily Coco <ec3307@nyu.edu>
2018-09-28 16:22:10 -0400

    added in shape/ so shapefile opens

--- code/1. Cleaning Data.R
+++ code/1. Cleaning Data.R
@@ -3,6 +3,6 @@
 #######################################
 
 library(sf)
-CT.boundaries <- st_read("2010_Census_Tracts/geo_export_bca342cd-a6e0-423a-849d-f4514a20112a.shp")
+CT.boundaries <- st_read("shape/2010_Census_Tracts/geo_export_bca342cd-a6e0-423a-849d-f4514a20112a.shp")
 
 
