b765bc0b22088ddf5877b531d2d398403b8f8537
Clare Clingain <cclingain@guttmacher.org>
2018-09-28 16:30:48 -0400

    Convert time and subset

--- code/1. Cleaning Data.R
+++ code/1. Cleaning Data.R
@@ -11,3 +11,12 @@ load("~/Github/stop_frisk/data/sqf.RData")
 
 # Subset to 2011
 sf2011 <- stops[stops$year==2011,]
+
+# Subset to 10 p.m. to 6 a.m.
+# Add non-important seconds place for time
+sf2011$time2 <- paste(sf2011$time, ":00")
+# First, we need to convert time to time
+require("chron")
+sf2011$time3 <- chron(times=sf2011$time2)
+
+sfnight <- sf2011[sf2011$time < 06:00:00 | sf2011$time > 22:00:00,]
