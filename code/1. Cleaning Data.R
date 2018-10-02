27a1c459111962314dd5d515f55cd62bc0135ba1
Emily Coco <ec3307@nyu.edu>
2018-10-02 17:24:04 -0400

    Added in correct library for reading demographic data

--- code/1. Cleaning Data.R
+++ code/1. Cleaning Data.R
@@ -3,6 +3,7 @@
 #######################################
 
 library(sf)
+library(acs)
 CT.boundaries <- st_read("shape/2010_Census_Tracts/geo_export_bca342cd-a6e0-423a-849d-f4514a20112a.shp")
 
 #get demographic data
