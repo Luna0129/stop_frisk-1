6c89cc7305fb6617fd1c018daed2a57c6ad9dd73
Emily Coco <ec3307@nyu.edu>
2018-10-05 15:48:39 -0400

    Further subsetting, added possible new idea with stops vs frisks

--- code/2. Calculate Rates.R
+++ code/2. Calculate Rates.R
@@ -2,7 +2,16 @@
 ########## 2. Calculate Rates #########
 #######################################
 
+
 # A. Clothing
+stop.clothing <- sffinal[sffinal$stopped.bc.clothing == TRUE,]
 
 
 # B. Furtive Movements
+stop.furtive <- sffinal[sffinal$stopped.bc.furtive == TRUE, ]
+
+
+# C. Stops vs. Frisks? because of Clothing/Furtive
+#could look at where stops because of a certain variable also turn into 
+#frisks and how that clusters
+
