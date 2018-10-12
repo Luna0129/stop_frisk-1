a64887cc999cb9e6f22f825dc8cb04692f2f4614
Alan Z Chen <alanzchen@nyu.edu>
2018-10-12 14:19:41 -0400

    keep sqf.ct as sf dataframe

--- code/2_calc_rates.r
+++ code/2_calc_rates.r
@@ -6,17 +6,16 @@ require(dplyr)
 
 # LOAD CLEANED DATA
 load('data/sqf2011_ct.rdata')
-sqf.ct <- as_tibble(sqf.ct)
 
 # CALCULATE RATES
-cols.stopped <- names(sqf.ct)[grep('^stopped', names(sqf.ct))]
-sqf.ct  <- sqf.ct %>% group_by(boro_ct201) %>% mutate(total = length(year))
+sqf.ct.grp  <- sqf.ct %>% group_by(boro_ct201) %>% mutate(total = length(year))
 
 # Rates
-rates <- sqf.ct %>%
+rates <- sqf.ct.grp %>%
     group_by(boro_ct201) %>%
-   summarise(stop.clothing=sum(stopped.bc.clothing)/mean(total),
+    summarise(stop.clothing=sum(stopped.bc.clothing)/mean(total),
         stop.furtive=sum(stopped.bc.furtive)/mean(total),
-        total_stopped=mean(total))
+        total_stopped=mean(total),
+        per_white=mean(per_white), per_black=mean(per_black), per_nat.amer=mean(per_nat.amer), per_asia=mean(per_asia), per_whisp=mean(per_whisp), per_bhisp=mean(per_bhisp), per_other=mean(per_other))
 
 
