f6d2861c0748faa347907409896e23a779b44b45
Alan Z Chen <alanzchen@nyu.edu>
2018-10-12 17:58:44 -0400

    generate nearest neighors, weights using SpatialPolygon from 1_clean.r; still issues with moran.mc differing lengths

--- code/3_global_tests.r
+++ code/3_global_tests.r
@@ -1,54 +1,20 @@
 ###############################
 ####### 3. Global Tests #######
 ###############################
-library(sf)
+library(rgdal)
 library(sp)
 library(spdep)
 library(spatstat)
 
+# Load CT rates data
+# "rates"
 load('data/rates.rdata')
 
+# Load CT SpatialPolygons
+# "ct.sp"
+load('data/ct.sp.rdata')
 
-
-autoCor <- function(geometry, crs, rateVar, test='moran', nsim, queen=FALSE) {
-    sp.poly <- as.SpatialPolygons.PolygonsList(list(geometry), crs)
-    nbs <- poly2nb(sp.poly, queen=queen)
-    lws <- nb2list2(nbs)
-
-    if (test == 'moran') {
-        return(moran.mc(rateVar, lws, nsim=nsim))
-    } else {
-        return(geary.mc(rateVar, lws, nsim=nsim))
-    }
-}
-
-
-# A. Rook Weighting
-# Pre-process data
-rates <- spTransform(PA.latlong, st_crs(rates))
-choropleth(PA.utm,PA.agg$rate)
-
-# Moran's I
-PA.nb <- poly2nb(PA.utm,queen=FALSE)
-PA.lw <- nb2listw(PA.nb)
-moran.test(PA.agg$adj.rate,PA.lw)
-moran.mc(PA.agg$adj.rate,PA.lw,nsim=999)
-
-# Geary's c
-geary.test(PA.agg$adj.rate,PA.lw)
-geary.mc(PA.agg$adj.rate,PA.lw,nsim=999)
-
-# B. Queen Weighting
-# Pre-process data
-PA.utm <- spTransform(PA.latlong,CRS("+init=epsg:3724 +units=km"))
-choropleth(PA.utm,PA.agg$rate)
-
-# Moran's I
-PA.nb <- poly2nb(PA.utm,queen=TRUE)
-PA.lw <- nb2listw(PA.nb)
-moran.test(PA.agg$adj.rate,PA.lw)
-moran.mc(PA.agg$adj.rate,PA.lw,nsim=999)
-
-# Geary's c
-geary.test(PA.agg$adj.rate,PA.lw)
-geary.mc(PA.agg$adj.rate,PA.lw,nsim=999)
+# Unit testing Moran's I (mc)
+nbs <- poly2nb(merged.sp)
+lws <- nb2listw(nbs, zero.policy=TRUE)
+moran.mc(rates$stop.clothing, lws, nsim=999, zero.policy=TRUE)
