8d490267649324271da21980b3c81e93544239e9
Clare Clingain <cclingain@guttmacher.org>
2018-10-15 10:35:10 -0400

    Fix rates calculations to match new data structure, get W matrix, remove the mismatched cases, run first moran's test

--- code/2_calc_rates.r
+++ code/2_calc_rates.r
@@ -5,10 +5,10 @@ require(sf)
 require(dplyr)
 
 # LOAD CLEANED DATA
-load('data/sqf2011_ct.rdata')
+load('data/final.rdata')
 
 # CALCULATE RATES
-sqf.ct.grp  <- sqf.ct %>% group_by(boro_ct201) %>% mutate(total = length(year))
+sqf.ct.grp  <- as.data.frame(final) %>% group_by(boro_ct201) %>% mutate(total = length(year))
 
 # Rates
 rates <- sqf.ct.grp %>%
@@ -17,6 +17,10 @@ rates <- sqf.ct.grp %>%
         stop.furtive=sum(stopped.bc.furtive)/mean(total),
         total_stopped=mean(total),
         per_white=mean(per_white), per_black=mean(per_black), per_nat.amer=mean(per_nat.amer), per_asia=mean(per_asia), per_whisp=mean(per_whisp), per_bhisp=mean(per_bhisp), per_other=mean(per_other))
+# This produces a data frame of 2136 tracts. However, tract 0 is the 19 points that
+# did not occur in any tract, so we will drop this row.
+rates <- rates[-1,]
 
+# Save the rates data
 save(rates, file='data/rates.rdata')
 
--- code/3_global_tests.r
+++ code/3_global_tests.r
@@ -13,7 +13,7 @@ load('data/rates.rdata')
 autoCor <- function(geometry, crs, rateVar, test='moran', nsim, queen=FALSE) {
     sp.poly <- as.SpatialPolygons.PolygonsList(list(geometry), crs)
     nbs <- poly2nb(sp.poly, queen=queen)
-    lws <- nb2list2(nbs)
+    lws <- nb2listw(nbs)
 
     if (test == 'moran') {
         return(moran.mc(rateVar, lws, nsim=nsim))
@@ -22,33 +22,38 @@ autoCor <- function(geometry, crs, rateVar, test='moran', nsim, queen=FALSE) {
     }
 }
 
+# I. Clothing
+## A. Rook Weighting
+### Pre-process data
+# Have to remove the tracts from lws not in data
+rm <- race_merged$boro_ct201 %in% rates$boro_ct201
+race_merged2 <- race_merged[rm,]
+# Get the W matrix
+nbs <- poly2nb(race_merged2, queen = F)
+lws <- nb2listw(nbs, zero.policy = T)
+choropleth(race_merged2,rates$stop.clothing)
 
-# A. Rook Weighting
-# Pre-process data
-rates <- spTransform(PA.latlong, st_crs(rates))
-choropleth(PA.utm,PA.agg$rate)
+### Moran's I
+moran.test(rates$stop.clothing,lws, zero.policy = T)
+moran.mc(rates$stop.clothing,lws,nsim=999, zero.policy = T)
 
-# Moran's I
-PA.nb <- poly2nb(PA.utm,queen=FALSE)
-PA.lw <- nb2listw(PA.nb)
-moran.test(PA.agg$adj.rate,PA.lw)
-moran.mc(PA.agg$adj.rate,PA.lw,nsim=999)
-
-# Geary's c
+### Geary's c
 geary.test(PA.agg$adj.rate,PA.lw)
 geary.mc(PA.agg$adj.rate,PA.lw,nsim=999)
 
-# B. Queen Weighting
-# Pre-process data
+## B. Queen Weighting
+### Pre-process data
 PA.utm <- spTransform(PA.latlong,CRS("+init=epsg:3724 +units=km"))
 choropleth(PA.utm,PA.agg$rate)
 
-# Moran's I
+### Moran's I
 PA.nb <- poly2nb(PA.utm,queen=TRUE)
 PA.lw <- nb2listw(PA.nb)
 moran.test(PA.agg$adj.rate,PA.lw)
 moran.mc(PA.agg$adj.rate,PA.lw,nsim=999)
 
-# Geary's c
+### Geary's c
 geary.test(PA.agg$adj.rate,PA.lw)
 geary.mc(PA.agg$adj.rate,PA.lw,nsim=999)
+
+# II. Furtive Movements
