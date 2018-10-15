de8afdbc617b5c90494f3bdc13d75166e9d459de
Clare Clingain <cclingain@guttmacher.org>
2018-10-15 11:19:29 -0400

    Add and save choropleth maps for local tests

--- code/4_local_tests.r
+++ code/4_local_tests.r
@@ -12,15 +12,20 @@ lws <- nb2listw(nbs, zero.policy = T)
 
 moran.loc.rook <-localmoran(rates$stop.clothing,lws,p.adjust.method="fdr", zero.policy = T) 
 shades <- shading(c(0.05),cols=c(2,8))
+png("Clothing.rooklocal.png")
 choropleth(race_merged2,moran.loc.rook[,"Pr(z > 0)"],shades)
-
+title("Rook's Continuity",cex=.8)
+dev.off()
 ## B. Queen Weighting
 nbs <- poly2nb(race_merged2, queen = T)
 lws <- nb2listw(nbs, zero.policy = T)
 
 moran.loc.queen <-localmoran(rates$stop.clothing,lws,p.adjust.method="fdr", zero.policy = T) 
 shades <- shading(c(0.05),cols=c(2,8))
+png("Clothing.queenlocal.png")
 choropleth(race_merged2,moran.loc.queen[,"Pr(z > 0)"],shades)
+title("Queen's Continuity",cex=.8)
+dev.off()
 
 # II. Furtive Movements
 ## A. Rook Weighting
@@ -30,12 +35,17 @@ lws <- nb2listw(nbs, zero.policy = T)
 
 moran.loc.rook <-localmoran(rates$stop.furtive,lws,p.adjust.method="fdr", zero.policy = T) 
 shades <- shading(c(0.05),cols=c(2,8))
+png("Furtive.rooklocal.png")
 choropleth(race_merged2,moran.loc.rook[,"Pr(z > 0)"],shades)
-
+title("Rook's Continuity",cex=.8)
+dev.off()
 ## B. Queen Weighting
 nbs <- poly2nb(race_merged2, queen = T)
 lws <- nb2listw(nbs, zero.policy = T)
 
 moran.loc.queen <-localmoran(rates$stop.furtive,lws,p.adjust.method="fdr", zero.policy = T) 
 shades <- shading(c(0.05),cols=c(2,8))
+png("Furtive.queenlocal.png")
 choropleth(race_merged2,moran.loc.queen[,"Pr(z > 0)"],shades)
+title("Queen's Continuity",cex=.8)
+dev.off()
